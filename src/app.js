44498bae75e71a727c4cba52db47ab05d62be0ca
aknsal <akshajkansal@yahoo.com>
2022-03-20 06:58:11 +0530

    update

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const helmet = require("helmet");
 const cors = require("cors");
+const path = require('path');
 const bodyParser = require("body-parser");
 require("dotenv").config();
 const cookieSession = require("cookie-session");
