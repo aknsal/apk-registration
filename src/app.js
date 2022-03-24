8f4f056ed49e4d98b4a2cfe1c8b1b5be1a650cea
aknsal <akshajkansal@yahoo.com>
2022-03-24 22:57:39 +0530

    update

--- src/app.js
+++ src/app.js
@@ -61,7 +61,7 @@ app.use("/api/", api);
 
 // Anything that doesn't match the above, send back the index.html file
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '..','client', 'build', 'index.html'))
+  res.sendFile(path.resolve(__dirname, '..','client', 'build', 'index.html'));
 })
 
 app.use(middlewares.notFound);
