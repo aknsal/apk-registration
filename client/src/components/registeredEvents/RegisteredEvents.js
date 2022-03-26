c303e840266f44731965b825b623d11a5900e1a5
aknsal <akshajkansal@yahoo.com>
2022-03-26 12:02:57 +0530

    update

--- client/src/components/registeredEvents/RegisteredEvents.js
+++ client/src/components/registeredEvents/RegisteredEvents.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 import EventCard from '../events/components/eventCard/EventCard';
 import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 export default function RegisteredEvents() {
 
@@ -20,13 +21,19 @@ export default function RegisteredEvents() {
           <Grid container spacing={3}>
             {
               user ?
+
+              (user &&
+                user.Events.length===0 ? 
+                <Typography > You have registered for no Events. Go an register to some exciting Events <Typography component={Link} to={"/events"}> here </Typography>  </Typography>
+                :
                 user.Events.map((event) => 
                   
                   <Grid key={event.id} item xs={12} sm={6} md={4} lg={3} >
                     <EventCard id={event.id} eventName={event.eventName} image={event.image1} eventCode={event.eventCode} />
                   </Grid>
                 )
-                :
+                )
+                : 
                 null
             }
           </Grid>
