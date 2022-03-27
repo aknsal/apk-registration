568ed2e68f81d82b43a2753810e4287f27733e91
aknsal <akshajkansal@yahoo.com>
2022-03-27 11:22:11 +0530

    update

--- client/src/pages/home/Homepage.js
+++ client/src/pages/home/Homepage.js
@@ -1,9 +1,14 @@
 import { Typography } from '@mui/material'
 import React from 'react'
 import "./Homepage.css"
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 
 export default function Homepage() {
+
+  const user = useSelector(state => state.app.authUser);
+
   return (
     <div className='homepage-super-container'>
       <div className='homepage-container'>
@@ -14,8 +19,17 @@ export default function Homepage() {
         <Typography variant='h4'> Annual Techfest of IIIT Allahabd </Typography>
         <span aria-hidden="true">Aparoksha</span>
       </p>
-
-      <Typography variant='h5'> Log in to get into the Rip Roaring World of Aparoksha. </Typography>
+      {
+        user?
+        <div>
+        <Typography variant='h5'> Welcome to the world of Aparoksha</Typography>
+        <Typography>Go on <Typography style={{textDecoration:"none", color:"#c1f5b8"}} component={Link} to={"/events"} color="success" textDecoration="none">Events Tab</Typography> and register for some events</Typography>
+        </div>
+        :
+        <Typography variant='h5'> Log in to get into the Rip Roaring World of Aparoksha. </Typography>
+        
+      }
+      
 
       </div>
     </div>
